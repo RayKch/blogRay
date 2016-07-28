bd492055478ea023aad473ae035cd536325c5ea9
Ray.Kim <rlacksgh08@naver.com>
2016-07-28 14:20:01 +0900

    modify : mapper 추가로 인하여 설정파일 주석 해제

--- src/main/java/ray/configuration/DatabaseConfig.java
+++ src/main/java/ray/configuration/DatabaseConfig.java
@@ -75,8 +75,8 @@ public class DatabaseConfig implements TransactionManagementConfigurer {
 	public SqlSessionFactory sqlSessionFactory() throws Exception {
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(dataSource());
-//		factoryBean.setConfigLocation(resourceLoader.getResource("classpath:sqlmap-config.xml"));
-//		factoryBean.setMapperLocations(patternResolver.getResources("classpath:mapper/*.xml"));
+		factoryBean.setConfigLocation(resourceLoader.getResource("classpath:sqlmap-config.xml"));
+		factoryBean.setMapperLocations(patternResolver.getResources("classpath:mapper/*.xml"));
 		return factoryBean.getObject();
 	}
 
