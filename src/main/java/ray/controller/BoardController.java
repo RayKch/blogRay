f61ca7cf2c81ec330b11552229574243e198470c
rlacksgh08 <rlacksgh0826@gmail.com>
2016-08-11 01:32:25 +0900

    modify : service 추가

--- src/main/java/ray/controller/BoardController.java
+++ src/main/java/ray/controller/BoardController.java
@@ -1,8 +1,10 @@
 package ray.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ray.service.BoardService;
 
 /**
  * Created by ChanPong on 2016-05-17.
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/board")
 public class BoardController {
+	@Autowired
+	private BoardService boardService;
+
 	@RequestMapping("/form")
 	public String form() {
 		return "/board/form.jsp";
--- src/main/java/ray/controller/CategoryController.java
+++ src/main/java/ray/controller/CategoryController.java
@@ -1,8 +1,10 @@
 package ray.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ray.service.CategoryService;
 
 /**
  * Created by BELL on 2016-08-10.
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/category")
 public class CategoryController {
+	@Autowired
+	private CategoryService categoryService;
+
 	@RequestMapping("/form")
 	public String form() {
 		return "/category/form.jsp";
