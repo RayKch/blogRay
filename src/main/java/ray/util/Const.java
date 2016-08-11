64f804f6f3336623e9a5425b30515e7e43d62895
Ray.Kim <rlacksgh08@naver.com>
2016-08-11 09:06:38 +0900

    add : return 페이지 사용관련 코드 추가

--- src/main/java/ray/util/Const.java
+++ src/main/java/ray/util/Const.java
@@ -3,14 +3,16 @@ package ray.util;
 public class Const {
 	public final static String VERSION = "2.7.4";
 
-	/** (등급) 최고 관리자 */
-	public final static int GRADE_SUPER_ADMIN = 1;
-
-	/** (등급) 작업자 */
-	public final static int GRADE_WORKER = 9;
-
-	/** (등급) 미승인 */
-	public final static int GRADE_NO_APPROVAL = 10;
+	/** 경고창만 뜨는 페이지 */
+	public final static String ALERT_PAGE = "/_proc/alert.jsp";
+	/** 경고창이 뜬 후에 부모창을 리다이렉트 시키는 페이지 */
+	public final static String REDIRECT_PAGE = "/_proc/parent_redirect.jsp";
+	/**  경고창이 뜬 후에 뒤로 되돌아가는 페이지 */
+	public final static String BACK_PAGE = "/_proc/back_page.jsp";
+	/** ajax호출용 빈 페이지 */
+	public final static String BLANK_PAGE = "/_proc/blank.jsp";
+	/** ajax호출 후에 메시지를 전달하는 페이지 */
+	public final static String AJAX_PAGE = "/_proc/ajax_message.jsp";
 
 	/** Aria 암호화 키 (반드시 32byte가 되어야만 한다) */
 	public final static byte[] ARIA_KEY = {
