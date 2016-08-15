e3101f5d0dd836c2f3cfe55da58b915b99e1b46c
rlacksgh08 <rlacksgh0826@gmail.com>
2016-08-15 15:57:31 +0900

    modify : 유효성 주석처리

--- src/main/java/ray/data/validator/MemberLoginValidator.java
+++ src/main/java/ray/data/validator/MemberLoginValidator.java
@@ -18,11 +18,11 @@ public class MemberLoginValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "id", "null", "아이디가 입력되지 않았습니다");
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "null", "비밀번호가 입력되지 않았습니다");
 
-		if(vo.getId() != null && !vo.getId().matches("^[a-zA-Z][a-zA-Z0-9\\.\\-_]{3,11}$")) {
-			errors.rejectValue("id", "match", "아이디가 유효하지 않습니다. 아이디는 영어로 시작하고 숫자와 특수문자(.-_)를 사용할 수 있습니다");
-		}
-		if(vo.getId() != null && (vo.getId().length() < 4 || vo.getId().length() > 12)) {
-			errors.rejectValue("id", "size", "아이디는 4~12자리까지 입력하실 수 있습니다");
-		}
+//		if(vo.getId() != null && !vo.getId().matches("^[a-zA-Z][a-zA-Z0-9\\.\\-_]{3,11}$")) {
+//			errors.rejectValue("id", "match", "아이디가 유효하지 않습니다. 아이디는 영어로 시작하고 숫자와 특수문자(.-_)를 사용할 수 있습니다");
+//		}
+//		if(vo.getId() != null && (vo.getId().length() < 4 || vo.getId().length() > 12)) {
+//			errors.rejectValue("id", "size", "아이디는 4~12자리까지 입력하실 수 있습니다");
+//		}
 	}
 }
