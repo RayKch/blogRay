86abd240f88e58e9288c2952a95099c78c2ee70c
rlacksgh08 <rlacksgh0826@gmail.com>
2016-08-15 14:49:34 +0900

    modify : 카테고리설명 컬럼 데이터타입 변경

--- src/main/resources/sql/category.sql
+++ src/main/resources/sql/category.sql
@@ -3,7 +3,7 @@ CREATE TABLE `ch_category` (
  `seq` int(11) NOT NULL AUTO_INCREMENT COMMENT '번호',
  `member_seq` int(11) NOT NULL COMMENT '회원 번호',
  `title` varchar(300) NOT NULL COMMENT '카테고리명',
- `description` text NOT NULL COMMENT '카테고리 설명',
+ `description` varchar(300) NOT NULL COMMENT '카테고리 설명',
  `reg_date` DATETIME NULL COMMENT '등록일',
  `mod_date` DATETIME NULL COMMENT '수정일',
  PRIMARY KEY (`seq`),
