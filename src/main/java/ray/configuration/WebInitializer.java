631c3cf31bafc282b8cd628e544d1b7ff26f9389
Ray.Kim <rlacksgh08@naver.com>
2016-08-18 14:29:22 +0900

    fix : form post 한글 데이터 전송시 깨짐 문제 필터추가로 수정

--- src/main/java/ray/configuration/WebInitializer.java
+++ src/main/java/ray/configuration/WebInitializer.java
@@ -2,11 +2,12 @@ package ray.configuration;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
+
+import javax.servlet.*;
+import java.util.EnumSet;
 
 /**
  * Created by ChanPong on 2016-05-17.
@@ -21,5 +22,12 @@ public class WebInitializer implements WebApplicationInitializer {
 		ServletRegistration.Dynamic appDispatcher;
 		appDispatcher = context.addServlet("app", new DispatcherServlet(dispatcherContext));
 		appDispatcher.addMapping("/");
+
+		// 인코딩 설정
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+		characterEncodingFilter.setEncoding("UTF-8");
+		characterEncodingFilter.setForceEncoding(true);
+		FilterRegistration.Dynamic characterEncoding = context.addFilter("characterEncodingFilter", characterEncodingFilter);
+		characterEncoding.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
 	}
 }
