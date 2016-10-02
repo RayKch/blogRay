863b36ec3529935fd77ed610881bfeec9dfa87fc
rlacksgh08 <rlacksgh0826@gmail.com>
2016-10-03 02:26:51 +0900

    modify : 파일 정보 테이블 컬럼 변경

--- src/main/resources/sql/system.sql
+++ src/main/resources/sql/system.sql
@@ -1,9 +1,10 @@
-/* 가입 회원(댓글은 모두 가능) */
+/* 파일 정보 */
 CREATE TABLE `ch_file` (
    `seq` int(11) NOT NULL AUTO_INCREMENT COMMENT '번호',
-   `path` varchar(300) NOT NULL COMMENT '경로',
+   `content_type` varchar(300) NOT NULL COMMENT '이미지 타입(jpg, jpeg, png, etc)',
+   `file_name` varchar(300) NOT NULL COMMENT '실제 파일이름',
    `board_seq` int(11) NULL COMMENT '게시글 시퀀스',
    `reg_date` DATETIME NULL COMMENT '등록일',
    PRIMARY KEY (`seq`),
    FOREIGN KEY(`board_seq`) REFERENCES ch_board(seq) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '파일 업로드'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT '파일 정보'
