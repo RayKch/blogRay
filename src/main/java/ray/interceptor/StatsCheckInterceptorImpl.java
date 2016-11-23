a1c7438d89efdadb40b7d7ef17122668060e4ce2
Ray.Kim <rlacksgh08@naver.com>
2016-11-23 13:49:00 +0900

    clean : 배너 관련 로직 refactoring

--- src/main/java/ray/interceptor/StatsCheckInterceptorImpl.java
+++ src/main/java/ray/interceptor/StatsCheckInterceptorImpl.java
@@ -15,8 +15,10 @@ import ray.service.StatsService;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.File;
 import java.util.Calendar;
 import java.util.Random;
+import java.util.Set;
 
 /**
  * 일일방문자를 체크해서 반환하는 인터셉터
@@ -46,34 +48,29 @@ public class StatsCheckInterceptorImpl extends HandlerInterceptorAdapter {
 		log.info("### requestURI : " + requestURI);
 
 		/** 히어로배너 자동변경 */
-		//각 계절별 이미지 보유 갯수
-		int spring = 3;
-		int summer = 1;
-		int autumn = 2;
-		int winter = 5;
-
 		//이미지 선택번호
 		int idx = 0;
 
 		//각 계절별 폴더 이름
 		String season = "";
 
+		//각 계절별 이미지 보유 갯수
 		Calendar cal = Calendar.getInstance();
 		int month = (cal.get(Calendar.MONTH) + 1);
 
 		Random r = new Random();
-		if(month >= 3 && month <=5) { //봄
+		if(month >= 3 && month <=5) { //봄(3~5월)
 			season = "spring";
-			idx = r.nextInt(spring) + 1;
-		} else if(month >= 6 && month <=8) { //여름
+			idx = r.nextInt(getMainFileCount(request, "spring")) + 1;
+		} else if(month >= 6 && month <=8) { //여름(6~8월)
 			season = "summer";
-			idx = r.nextInt(summer) + 1;
-		} else if(month >= 9 && month <=11) { //가을
+			idx = r.nextInt(getMainFileCount(request, "summer")) + 1;
+		} else if(month >= 9 && month <=11) { //가을(9~11월)
 			season = "autumn";
-			idx = r.nextInt(autumn) + 1;
-		} else if(month >= 12 && month <=2) { //겨울
+			idx = r.nextInt(getMainFileCount(request, "autumn")) + 1;
+		} else if(month >= 12 && month <=2) { //겨울(12~2월)
 			season = "winter";
-			idx = r.nextInt(winter) + 1;
+			idx = r.nextInt(getMainFileCount(request, "winter")) + 1;
 		}
 
 		request.setAttribute("season", season);
@@ -95,5 +92,11 @@ public class StatsCheckInterceptorImpl extends HandlerInterceptorAdapter {
 //			}
 //		}
 		return true;
+	}
+
+	/** 각 계절별 폴더에 있는 파일들의 총 갯수 반환 */
+	private int getMainFileCount(HttpServletRequest request, String typeCode) {
+		File path = new File(request.getServletContext().getRealPath("/") + "image" + File.separator + "main" + File.separator + typeCode);
+		return path.listFiles().length;
 	}
 }
